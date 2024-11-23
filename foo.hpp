3c741dabbee110745528f1cb417f95d740df23e0
Jędrzej <mr.myszer@gmail.com>
2024-11-23 17:39:30 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,18 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > result;
+    for (std::list< Human >::iterator lit = people.begin(); lit != people.end(); lit++)
+    {
+        lit->birthday();
+    }
+    for (std::list<Human>::reverse_iterator lit2 = people.rbegin(); lit2 != people.rend(); lit2++)
+    {
+        if (lit2->isMonster() == false) {
+            result.emplace_back('y');
+        }
+        else
+            result.emplace_back('n');
+    }
+    return result;
 }
